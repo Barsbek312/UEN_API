cb520ae280cbdf8464586f4adaed6138bae86a25
eldosagai <eldarasanbakov@mail.ru>
2024-04-10 22:45:57 +0600

    cors addded

--- UEN/settings.py
+++ UEN/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
+from corsheaders.defaults import default_headers
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -152,6 +153,20 @@ REST_FRAMEWORK = {
     ),
 }
 
+CORS_ORIGIN_ALLOW_ALL = False
+
+# Изменения Барсбека
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8000',  # Add other allowed origins if needed
+]
+
+CORS_ALLOW_HEADERS = list(default_headers) + [
+    'access-control-allow-origin',
+    'access-control-allow-methods',
+    'access-control-allow-credentials',
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
